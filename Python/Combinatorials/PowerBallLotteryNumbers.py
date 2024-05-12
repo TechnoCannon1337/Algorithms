43641c7dd88f1d3ee8fb16b079efe723ba54843a
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-11 18:15:57 -0700

    Create PowerBallLotteryNumbers.py

---
+++ Python/Combinatorials/PowerBallLotteryNumbers.py
@@ -0,0 +1,7 @@
+for A in range(1, 66):
+    for B in range(A + 1, 67):
+        for C in range(B + 1, 68):
+            for D in range(C + 1, 69):
+                for E in range(D + 1, 70):
+                    for F in range(1, 27):
+                        print(str(A)+','+str(B)+','+str(C)+','+str(D)+','+str(E)+','+str(F))
