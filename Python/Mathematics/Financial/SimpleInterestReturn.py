06118b679ab0e30312317ea944d727ecba4f3b96
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-15 12:17:07 -0700

    Create SimpleInterestReturn.py

---
+++ Python/Mathematics/Financial/SimpleInterestReturn.py
@@ -0,0 +1,4 @@
+def simpleInterestReturn(principleInvestment, interestRate, annualDuration):
+    return(round(principleInvestment + (principleInvestment * interestRate * annualDuration), 2))
+
+simpleInterestReturn()
