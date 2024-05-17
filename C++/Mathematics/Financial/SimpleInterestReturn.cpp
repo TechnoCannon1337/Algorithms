f1aaa953de63821a777214c24cd11fd5d6b322e4
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-17 07:30:50 -0700

    Update SimpleInterestReturn.cpp

--- C++/Mathematics/Financial/SimpleInterestReturn.cpp
+++ C++/Mathematics/Financial/SimpleInterestReturn.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include "financialFunctions.h"
 using namespace std;
 
 float simpleInterestReturn(float principleInvestment, float interestRate, float annualDuration)
@@ -7,8 +8,4 @@ float simpleInterestReturn(float principleInvestment, float interestRate, float 
   float interestPaid = principleInvestment * interestRate;
   float totalReturn = principleInvestment + (interestPaid * annualDuration);
   return totalReturn;
-}
-int main()
-{
-  simpleInterestReturn();
 }
