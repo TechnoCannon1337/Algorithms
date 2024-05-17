bc42d3de615c48d374331a528120b053da7e7dbc
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-17 09:10:58 -0700

    Update main.cpp

--- C++/Mathematics/Financial/main.cpp
+++ C++/Mathematics/Financial/main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main(){
 
+cout.setf(ios::fixed);
+cout.setf(ios::showpoint);
+cout.precision(2);
 cout << "The Simple Interest Return of a Principle Investment of $42,000 at a 7% Interest Rate over 14 Years is $" << simpleInterestReturn(42000, .07, 14) <<endl;
 cout << "The Compound Interest Return of a Principle Investment of $42,000 at a 7% Interest Rate compounded Quarterly over 14 Years is $" << compoundInterestReturn(42000, .07, 4, 14) <<endl;
 cout << "The Present Value of a Lump Sum Payment of $42,000 at a 7% Interest Rate compounded Quarterly over 14 Years is $" << lumpSumPresentValue(42000, .07, 4, 14) <<endl;
