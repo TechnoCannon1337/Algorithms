45880942af4e15db993d841c947d56ab0e9da778
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-24 23:51:38 -0700

    Add files via upload

--- C++/Mathematics/Financial/main.cpp
+++ C++/Mathematics/Financial/main.cpp
@@ -1,36 +1,39 @@
-#include <iostream>
 #include <locale>
 #include <iomanip>
-#include <string>
 #include "financialFunctions.h"
 
 using namespace std;
 
 int main(){
 
-
 SIMPLEINTEREST testSIMPLEINTEREST;
 COMPOUNDINTEREST testCOMPOUNDINTEREST;
 PRESENTLUMPSUM testPRESENTLUMPSUM;
 FUTURELUMPSUM testFUTURELUMPSUM;
 PRESENTANNUITY testPRESENTANNUITY;
+GLOSSARY testGLOSSARY;
 
+testSIMPLEINTEREST.setsimpleInterestReturn(42000, .07, 14);
+testCOMPOUNDINTEREST.setcompoundInterestReturn(42000, .07, 4, 14);
+testPRESENTLUMPSUM.setlumpSumPresentValue(42000, .07, 4, 14);
+testFUTURELUMPSUM.setlumpSumFutureValue(42000, .07, 4, 14);
+testPRESENTANNUITY.setannuityPresentValue(42000, .14, 7);
 
 cout.imbue(locale("en_US.UTF-8"));
-cout << showbase << "\nThe Simple Interest Return of a Principle Investment of $42,000 at a 7% Interest Rate over 14 Years is " << put_money(testSIMPLEINTEREST.simpleInterestReturn(42000, .07, 14)*100) <<"."<<endl;
-cout <<"Definition: " << testSIMPLEINTEREST.vocabulary()<< "\n"<< endl;
+cout << showbase << "\nThe Simple Interest Return of a Principle Investment of $42,000 at a 7% Interest Rate over 14 Years is " << put_money(testSIMPLEINTEREST.getsimpleInterestReturn()*100) <<"."<<endl;
+cout <<"Definition: " << testGLOSSARY.definesimpleInterestReturn()<< "\n"<< endl;
 
-cout << "The Compound Interest Return of a Principle Investment of $42,000 at a 7% Interest Rate compounded Quarterly over 14 Years is " << put_money(testCOMPOUNDINTEREST.compoundInterestReturn(42000, .07, 4, 14)*100) <<"."<<endl;
-cout <<"Definition: " << testCOMPOUNDINTEREST.vocabulary()<< "\n"<< endl;
+cout << "The Compound Interest Return of a Principle Investment of $42,000 at a 7% Interest Rate compounded Quarterly over 14 Years is " << put_money(testCOMPOUNDINTEREST.getcompoundInterestReturn()*100) <<"."<<endl;
+cout <<"Definition: " << testGLOSSARY.definecompoundInterestReturn()<< "\n"<< endl;
 
-cout << "The Present Value of a Lump Sum Payment of $42,000 at a 7% Interest Rate compounded Quarterly over 14 Years is " << put_money(testPRESENTLUMPSUM.lumpSumPresentValue(42000, .07, 4, 14)*100) <<"."<<endl;
-cout <<"Definition: " << testPRESENTLUMPSUM.vocabulary()<< "\n"<< endl;
+cout << "The Present Value of a Lump Sum Payment of $42,000 at a 7% Interest Rate compounded Quarterly over 14 Years is " << put_money(testPRESENTLUMPSUM.getlumpSumPresentValue()*100) <<"."<<endl;
+cout <<"Definition: " << testGLOSSARY.definelumpSumPresentValue()<< "\n"<< endl;
 
-cout <<  "The Future Value of a Lump Sum Payment of $42,000 at a 7% Interest Rate compounded Quarterly over 14 Years is "<< put_money(testFUTURELUMPSUM.lumpSumFutureValue(42000, .07, 4, 14)*100) <<"."<<endl;
-cout <<"Definition: " << testFUTURELUMPSUM.vocabulary()<< "\n"<< endl;
+cout <<  "The Future Value of a Lump Sum Payment of $42,000 at a 7% Interest Rate compounded Quarterly over 14 Years is "<< put_money(testFUTURELUMPSUM.getlumpSumFutureValue()*100) <<"."<<endl;
+cout <<"Definition: " << testGLOSSARY.definelumpSumFutureValue()<< "\n"<< endl;
 
-cout << "The Annuity Present Value of Cash Flow Payments of $42,000 at a 14% Interest Rate over 7 Years is " << put_money(testPRESENTANNUITY.annuityPresentValue(42000, .14, 7)*100) <<"."<<endl;
-cout <<"Definition: " << testPRESENTANNUITY.vocabulary()<< endl;
+cout << "The Annuity Present Value of Cash Flow Payments of $42,000 at a 14% Interest Rate over 7 Years is " << put_money(testPRESENTANNUITY.getannuityPresentValue()*100) <<"."<<endl;
+cout <<"Definition: " << testGLOSSARY.defineannuityPresentValue()<< endl;
 
 cout << "\n\n" <<"PRESS ENTER TO EXIT THIS PROGRAM." << endl;
 
