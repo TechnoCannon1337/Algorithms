0cf405b481bc729601359ea0ed77f5b23cbaa23a
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-24 23:51:55 -0700

    Add files via upload

--- C++/Mathematics/Financial/Headers/financialFunctions.h
+++ C++/Mathematics/Financial/Headers/financialFunctions.h
@@ -1,36 +1,68 @@
 #ifndef FINANCIALFUNCTIONS_H
 #define FINANCIALFUNCTIONS_H
+#include <iostream>
+#include <cmath>
 #include <string>
 using namespace std;
 
 class SIMPLEINTEREST {
+  private:
+    double totalReturn;
+    void simpleInterestReturn(double principleInvestment, double interestRate, double annualDuration);
   public:
-    double simpleInterestReturn(double principleInvestment, double interestRate, double annualDuration);
-    string vocabulary();
+    void setsimpleInterestReturn(double setprincipleInvestment, double setinterestRate, double setannualDuration);
+    double getsimpleInterestReturn();
 };
 
 class COMPOUNDINTEREST {
+  private:
+    double totalReturn;
+    void compoundInterestReturn(double principleInvestment, double interestRate, double compoundPerYear, double annualDuration);
   public:
-    double compoundInterestReturn(double principleInvestment, double interestRate, double compoundPerYear, double annualDuration);
-    string vocabulary();
+    void setcompoundInterestReturn(double setprincipleInvestment, double setinterestRate, double setcompoundPerYear, double setannualDuration);
+    double getcompoundInterestReturn();
 };
 
 class PRESENTLUMPSUM {
+  private:
+    double presentValue;
+    void lumpSumPresentValue(double lumpSum, double annualInterestRate, double compoundingPeriods, double timeSpan);
   public:
-    double lumpSumPresentValue(double lumpSum, double annualInterestRate, double compoundingPeriods, double timeSpan);
-    string vocabulary();
+    void setlumpSumPresentValue(double setlumpSum, double setannualInterestRate, double setcompoundingPeriods, double settimeSpan);
+    double getlumpSumPresentValue();
 };
 
 class FUTURELUMPSUM {
+  private:
+    double futureValue;
+    void lumpSumFutureValue(double lumpSum, double annualInterestRate, double compoundingPeriods, double timeSpan);
   public:
-    double lumpSumFutureValue(double lumpSum, double annualInterestRate, double compoundingPeriods, double timeSpan);
-    string vocabulary();
+    void setlumpSumFutureValue(double setlumpSum, double setannualInterestRate, double setcompoundingPeriods, double settimeSpan);
+    double getlumpSumFutureValue();
 };
 
 class PRESENTANNUITY {
+  private:
+    double PresentValueAnnuity;
+    void annuityPresentValue(double Payment, double rate, double timeSpan);
   public:
-    double annuityPresentValue(double Payment, double rate, double timeSpan);
-    string vocabulary();
+    void setannuityPresentValue(double setPayment, double setrate, double settimeSpan);
+    double getannuityPresentValue();
+};
+
+class GLOSSARY {
+  private:
+    static string simpleInterestDefined;
+    static string compoundInterestDefined;
+    static string lumpSumPresentValueDefined;
+    static string lumpSumFutureValueDefined;
+    static string PresentValueAnnuityDefined;
+  public:
+    string definesimpleInterestReturn();
+    string definecompoundInterestReturn();
+    string definelumpSumPresentValue();
+    string definelumpSumFutureValue();
+    string defineannuityPresentValue();
 };
 
 #endif
