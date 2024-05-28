95c0d3e16915b63ef02ae70271befc28ad92698e
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-27 18:32:34 -0700

    Update osWalkThroughSubdirectories.py

--- Python/Traversals/osWalkThroughSubdirectories.py
+++ Python/Traversals/osWalkThroughSubdirectories.py
@@ -3,10 +3,14 @@ import os.path
 import sys
 
 def main(sourceDirectory):
-    for path, folders, fileNames in os.walk(sourceDirectory):
-        for file in fileNames:
-            with open(os.path.join(path, file), 'r') as sourceFile:
-                with open(file, 'a') as writePath:
-                    writePath.write(sourceFile.read())
+	for path, folders, fileNames in os.walk(sourceDirectory):
+		os.makedirs(os.getcwd()+path, exist_ok=True)
+		for file in fileNames:
+			with open(os.path.join(path, file), 'r') as sourceFile:
+				currentDirectory = os.getcwd()
+				newPath = currentDirectory+path
+				with open(os.path.join(newPath, file), 'a') as writePath:
+					writePath.write(sourceFile.read())
 
-main('/path/to/directory')
+targetDirectory = '/path/to/directory'
+main(targetDirectory)
