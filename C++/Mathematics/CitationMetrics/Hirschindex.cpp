49220d60a03ec40a5e3d4a991072f102d6dc39c4
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-30 07:13:30 -0700

    Create Hirschindex.cpp

---
+++ C++/Mathematics/CitationMetrics/Hirschindex.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+/*
+The h-index is an author-level metric that measures both the productivity and citation impact of the publications, initially used for an individual scientist or scholar. The h-index correlates with success indicators such as winning the Nobel Prize, being accepted for research fellowships and holding positions at top universities.
+
+The index is based on the set of the scientist's most cited papers and the number of citations that they have received in other publications.
+
+The index was suggested in 2005 by Jorge E. Hirsch, a physicist at UC San Diego, as a tool for determining theoretical physicists' relative quality
+
+The h-index is defined as the maximum value of h such that the given author/journal has published at least h papers that have each been cited at least h times
+
+*/
+
+double class HIndexCalculator() {
+private:
+  double publication_Count;
+  double citation_Count;
+  double running_H_Index_Count;
+  double H_Index_Summation;
+  double H_Index_Array[2][publication_Count];
+public:
+  void printString(string output_String){
+    cout << output_String << endl;
+  }
+  bool getUserInput(){
+    bool number_Count;
+    cin >> number_Count;
+    return number_Count;
+  }
+
+  void setData(){
+    printString("Enter Total Number Publication Titles");
+    publication_Count = getUserInput();
+    for (int i=0; i < 2; i++){
+      for (int j; j < publication_Count; j++){
+        printString("Enter Total Number of Citations for Publication number " << j);
+        H_Index_Array[0][j] = j;
+        H_Index_Array[1][j] = getUserInput();
+
+        //Todo: Develop MergeSort Function to order from Max to Min Values
+
+        if (H_Index_Array[1][j] >= H_Index_Array[0][j]){
+        	H_Index_Summation = H_Index_Array[0][j];
+        } else {
+        	break;
+        }
+      }
+    }
+  }
+
+  double getData(){
+    printString("The h-index is " << H_Index_Summation);
+    return H_Index_Summation;
+  }
+
+};
+
+
+int main()
+{
+  HIndexCalculator testHIndexCalculator;
+  testHIndexCalculator();
+
+
+
+};
