4db3788dc4ec00195a97cb90ce6652b353352e77
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-31 07:22:47 -0700

    Update Hirschindex.cpp

--- C++/Mathematics/CitationMetrics/Hirschindex.cpp
+++ C++/Mathematics/CitationMetrics/Hirschindex.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <string>
 #include <cstring>
-// Reorder QuickSort
+//Todo Debug
 using namespace std;
 
 /*
@@ -22,7 +22,7 @@ private:
   int citation_Count;
   int running_H_Index_Count;
   int H_Index_Summation;
-  int H_Index_Array[2][1];
+  int *H_Index_Array = new int[2][1];
 
 public:
   void printString(string output_String, int output_Data ='\0'){
@@ -35,10 +35,20 @@ public:
     return number_Count;
   }
 
+  size_t resizeHindexArray(int **array, size_t oldsize, size_t amount){
+    size_t new_size = oldsize + amount;
+    int *new_array = new int[2][new_size];
+    memcpy(new_array, array, oldsize * sizeof(int));
+    delete [] *array;
+    *array = new_array;
+    return new_size;
+
+  }
+
   void setData(){
     printString("Enter Total Number Publication Titles");
     publication_Count = getUserInput();
-
+    resizeHindexArray(&H_Index_Array, 1, publication_Count);
     for (int i=0; i < 2; i++){
       for (int j=0; j < publication_Count; j++){
         printString("Enter Total Number of Citations for Publication number " , j);
@@ -89,7 +99,7 @@ public:
     printString("The h-index is ", H_Index_Summation);
     return H_Index_Summation;
   }
-
+delete [] H_Index_Array);
 };
 
 
