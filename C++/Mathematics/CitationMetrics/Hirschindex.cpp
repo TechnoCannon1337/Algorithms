49589e8aec34db1e713296357edcfab55b62944d
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-05-31 23:25:37 -0700

    Update Hirschindex.cpp

--- C++/Mathematics/CitationMetrics/Hirschindex.cpp
+++ C++/Mathematics/CitationMetrics/Hirschindex.cpp
@@ -1,121 +1,59 @@
 #include <iostream>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
 class HIndexCalculator {
-
 private:
-    int **H_Index_Array;
-    int publication_count;
-    int citation_Count;
-    int H_Index_Summation;
-    size_t current_size;
-
-public:
-    HIndexCalculator();
-    ~HIndexCalculator();
-    void printString(string output_String, int output_Data);
-    int getUserInput();
-    void setData();
-    int partition(int** arr, int left, int right);
-    void sortHIndex(int** arr, int left, int right);
-    void calculateHIndex();
-    int getData();
-
+static const int publication_Count = 6;
+int H_Index_Array[2][publication_Count] = {
+  {1, 2, 3, 4, 5, 6},
+  {5, 25, 8, 3, 3, 5}
 };
 
-HIndexCalculator::HIndexCalculator() : H_Index_Array(nullptr), publication_count(0), current_size(0) {}
-
-HIndexCalculator::~HIndexCalculator() {
-    for (size_t i = 0; i < 2; ++i) {
-        delete[] H_Index_Array[i];
+public:
+int partition(int arr[2][publication_Count], int left, int right){
+  int pivot = arr[1][(left + right)/2];
+  while (left <= right){
+    while (arr[1][left] < pivot) left++;
+    while (arr[1][right] > pivot) right--;
+    if (left <= right){
+      int temp_swap = arr[1][left];
+      arr[1][left] = arr[1][right];
+      arr[1][right] = temp_swap;
+      left++;
+      right--;
     }
-    delete[] H_Index_Array;
+  }
+  return left;
 }
 
-void HIndexCalculator::printString(string output_String, int output_Data = '\0') {
-  if (output_Data == '\0'){
-    cout << output_String << endl;
-  } else{
-    cout << output_String << output_Data << endl;
+void sortHIndex(int arr[2][publication_Count], int left, int right){
+  int index = partition(arr, left, right);
+  if (left < index - 1){
+    sortHIndex(arr, left, index - 1);
+  }
+  if (index < right) {
+    sortHIndex(arr, index, right);
   }
 }
 
-int HIndexCalculator::getUserInput() {
-    int input;
-    cin >> input;
-    return input;
+void startQuickSort(){
+sortHIndex(H_Index_Array, 0, publication_Count -1);
+for (int i=0; i < 1; i++){
+  for (int j=0; j < publication_Count; j++){
+    cout << H_Index_Array[1][j] << " ";
+  }
 }
 
-int HIndexCalculator::partition(int** arr, int left, int right) {
-    int pivot = arr[1][(left + right) / 2];
-    while (left <= right) {
-        while (arr[1][left] > pivot) left++;
-        while (arr[1][right] < pivot) right--;
-        if (left <= right) {
-            int temp_swap = arr[1][left];
-            arr[1][left] = arr[1][right];
-            arr[1][right] = temp_swap;
-            left++;
-            right--;
-        }
-    }
-    return left;
 }
 
-void HIndexCalculator::sortHIndex(int** arr, int left, int right) {
-    int index = partition(arr, left, right);
-    if (left < index - 1) {
-        sortHIndex(arr, left, index - 1);
-    }
-    if (index < right) {
-        sortHIndex(arr, index, right);
-    }
-}
+};
 
-void HIndexCalculator::setData() {
-    printString("ENTER TOTAL NUMBER OF PUBLICATION TITLES AS NUMERICAL INTEGERS AND PRESS ENTER.");
-    publication_count = getUserInput();
+int main(){
+HIndexCalculator testHIndexCalculator;
+testHIndexCalculator.startQuickSort();
+return 0;
 
-    H_Index_Array = new int*[2];
-    for (size_t i = 0; i < 2; ++i) {
-        H_Index_Array[i] = new int[publication_count];
-        int* new_row = new int[publication_count];
-        for (size_t j = 0; j < publication_count; ++j) {
-            new_row[j] = H_Index_Array[i][j];
-        }
-        delete[] H_Index_Array[i];
-        H_Index_Array[i] = new_row;
-    }
-    current_size = publication_count;
-
-    for (int j = 0; j < publication_count; j++) {
-        printString("ENTER TOTAL NUMBER OF CITATIONS AS NUMERICAL INTERGERS FOR PUBLICATION NUMBER " , j + 1);
-        H_Index_Array[0][j] = j;
-        H_Index_Array[1][j] = getUserInput();
-    }
-    sortHIndex(H_Index_Array, 0, publication_count - 1);
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < publication_count; j++) {
-            if (H_Index_Array[1][j] >= H_Index_Array[0][j]) {
-                H_Index_Summation = H_Index_Array[0][j];
-            } else {
-                break;
-            }
-        }
-    }
-}
-
-int HIndexCalculator::getData() {
-    printString("THE H-INDEX IS ", H_Index_Summation);
-    return H_Index_Summation;
-}
-
-int main() {
-    HIndexCalculator testHIndexCalculator;
-    testHIndexCalculator.setData();
-    testHIndexCalculator.getData();
-    testHIndexCalculator.printString("PRESS ENTER TO EXIT THIS PROGRAM.");
-    cin.get();
-    return 0;
 }
